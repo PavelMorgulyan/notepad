987993e5e8a7bda0be9c102d262d497da3152537
PavelMorgulyan <74529349+PavelMorgulyan@users.noreply.github.com>
2021-12-08 11:38:23 +0300

    add admin statics style

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ python manage.py makemigrations --no-input
 
 python manage.py migrate --no-input
 
+python manage.py collectstatic --no-input
+
 exec gunicorn notes.wsgi:application -b 0.0.0.0:8000 --reload
